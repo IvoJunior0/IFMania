7267d26cbf06e861a03ffa90c6c660d780c8098d
Ivo Junior <shymanjr2019@gmail.com>
2025-04-15 23:21:31 -0300

    precisão não passa mais dos 100%

--- js/game.js
+++ js/game.js
@@ -42,7 +42,7 @@ function parseMapa(str) {
     });
 }
 // FIXME: Esse é só o de Helix, dps criar outros arquivos com as notas e os ms de cada nota.
-const mapaStr = "2745|1,3431|0,4117|2,4803|3,5488|1,6174|2,6860|0,7546|0,7888|2,8060|1,8231|3,8574|2,8917|0,9260|1,9603|2,9946|3,10288|1,10631|0,10974|1,11146|2,11317|1,11488|2,11660|3,11831|0,12003|3,12174|0,12346|2,12517|3,12688|1,12860|2,13031|0|s:343";
+const mapaStr = "2745|1,3431|0,4117|2,4803|3,5488|1,6174|2,6860|0,7546|0,7888|2,8060|1,8231|3,8574|2,8917|0,9260|1,9603|2,9946|3,10288|1,10631|0,10974|1,11146|2,11317|1,11488|2,11660|3,11831|0,12003|3,12174|0,12346|2,12517|3,12688|1,12860|2,13031|0|s:343,13717|1,14060|3,14403|0,14746|2,15088|3,15431|0,15774|2,16117|1,16460|0,16803|3,17146|1,17488|2,17831|3,18174|0,18517|3,18860|1,19031|2,19203|3,19546|0,19888|2,20231|1,20574|3,20917|2,21603|0,21946|3,22288|2,22631|1,22974|2,23317|0,23660|3,24003|0,24346|2,24688|2,25031|0,25374|1,25717|3,26060|2,26231|1,26403|2,26574|1,26746|3,26917|0,27088|3,27260|0,27431|2,27774|1,27946|2,28288|0,28460|2,28803|0,28974|1,29146|3,29317|2,29488|1,29660|0,29831|3,30003|2";
 const mapa = parseMapa(mapaStr);
 
 // Elementos do HTML.
@@ -63,7 +63,7 @@ let combo = 0;
 let acertosQtd = [0, 0, 0, 0, 0, 0]; // Cada index representa os misses, 50, 100, 200, 300 e 320 respectivamente.
 let hitsQtdTotal = 0;
 let pontuacaoTotal = 0;
-let precisao;
+let precisao = 100;
 
 // Menor valor que a nota pode chegar
 const topMax = 600;
@@ -160,9 +160,8 @@ function verificarAcerto(coluna) {
         hitsQtdTotal++;
     }
 
-    pontuacaoTotal = acertosQtd[1] * 50 + acertosQtd[2] * 100 + acertosQtd[3] * 200 + acertosQtd[4] * 300 + acertosQtd[5] * 320;
+    pontuacaoTotal = acertosQtd[1] * 50 + acertosQtd[2] * 100 + acertosQtd[3] * 200 + acertosQtd[4] * 300 + acertosQtd[5] * 300;
     precisao = (pontuacaoTotal / (hitsQtdTotal * 300)) * 100;
-    if (precisao > 100) precisao = 10;
 
     comboDiv.innerHTML = combo;
     accuracyDiv.innerHTML = precisao.toFixed(2);
